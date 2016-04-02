eacd65d6b94a8ec16024bc56c01d8af3bf49b687
kioshimafx <kioshimafx@gmail.com>
2016-04-03 00:36:39 +0300

    new :  create search controller and remove injection

--- FXHydra/main.m
+++ FXHydra/main.m
@@ -8,10 +8,10 @@
 
 // From here to end of file added by Injection Plugin //
 
-#ifdef DEBUG
-static char _inMainFilePath[] = __FILE__;
-static const char *_inIPAddresses[] = {"192.168.1.126", "127.0.0.1", 0};
-
-#define INJECTION_ENABLED
-#import "/tmp/injectionforxcode/BundleInjection.h"
-#endif
+//#ifdef DEBUG
+//static char _inMainFilePath[] = __FILE__;
+//static const char *_inIPAddresses[] = {"192.168.1.126", "127.0.0.1", 0};
+//
+//#define INJECTION_ENABLED
+//#import "/tmp/injectionforxcode/BundleInjection.h"
+//#endif
